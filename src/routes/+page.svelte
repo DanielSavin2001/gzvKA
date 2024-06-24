e9debc682fe4a417d1bdd3f368648a00012fdc1b
DanielSavin2001 <DanielSavin2001@users.noreply.github.com>
2024-06-24 19:23:12 +0000

    deploy: 5a84ebd3f27dd1514409a9090e09bdcb667ce372

--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div class="text-center">
-    <h1 class="m-4 mt-16 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white max-w-5xl mx-auto">Welkom tot <b>Ge zijt van Kapellen als ge ...</b> fotoarchief!</h1>
-    <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-        Duik in de rijke geschiedenis van Kapellen met onze unieke verzameling foto's van bijzondere plekken, mensen en momenten.
-    </p>
-</div>
