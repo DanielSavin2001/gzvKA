9d3e7ca197feec44d0f81d1ea032dfe318bd9b99
DanielSavin2OO1 <daniel.savin2001@outlook.com>
2024-08-07 01:36:53 +0200

    Chore: Initial commit related to Firebase hosting/functions/storage.

---
+++ functions/src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import { Request, Response } from 'express';
+// import * as logger from 'firebase-functions/logger';
+// import * as functions from 'firebase-functions';
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,25 @@
+<script>
+    import { MapLibre } from 'svelte-maplibre';
+</script>
+
 <div class="text-center">
     <h1 class="m-4 mt-16 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white max-w-5xl mx-auto">Welkom tot <b>Ge zijt van Kapellen als ge ...</b> fotoarchief!</h1>
     <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
         Duik in de rijke geschiedenis van Kapellen met onze unieke verzameling foto's van bijzondere plekken, mensen en momenten.
     </p>
 </div>
+
+<br>
+<br>
+<MapLibre
+        center={[4.369681, 51.307764]}
+        zoom={10}
+        class="map"
+        standardControls
+        style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json" />
+
+<style>
+    :global(.map) {
+        min-height: 500px;
+    }
+</style>
